518c3313493a223a843154eb071334d591c2f38e
Akinyemi Oluwabukunmi <braingrams40@gmail.com>
2022-07-12 20:11:06 +0100

    Fixed display on mobile

--- src/lib/components/Utilities/HideAdmin.tsx
+++ src/lib/components/Utilities/HideAdmin.tsx
@@ -6,9 +6,9 @@ function HideAdmin() {
 			w="full"
 			bgColor="brand.100"
 			h="100vh"
+			overflow="hidden"
 			justifyContent="center"
 			alignItems="center"
-			px={16}
 			flexDirection="column"
 		>
 			<Circle size="6rem" bgColor="white" p=".3rem" border="5px solid white">
@@ -18,10 +18,10 @@ function HideAdmin() {
 				<Text fontSize="1.2rem" fontWeight="600" mb="1rem">
 					Oh, hi there!
 				</Text>
-				<Text fontSize=".9rem" fontWeight="400">
+				<Text fontSize="1rem" fontWeight="400" w="80%" mx="auto">
 					We currently do not support admin view on this screen resolution.
 					Please manage your admin dashboard on a different device, or if you're
-					using a laptop, try maximixing it or use a bigger screen
+					using a desktop, try maximixing to full screen or use a bigger screen
 				</Text>
 			</Box>
 		</Flex>
